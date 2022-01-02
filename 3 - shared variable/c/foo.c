1560995e1419c4f074455ca61975d2d4d54eba7c
Anders Petersen <klasbo@gmail.com>
2022-01-02 21:19:58 +0100

    2022 Init

---
+++ 3 - shared variable/c/foo.c
@@ -0,0 +1,31 @@
+// Compile with `gcc foo.c -std=c99 -lpthread`, or use the makefile
+
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+// Note the return type: void*
+void* incrementingThreadFunction(){
+    // TODO: increment i 1_000_000 times
+    return NULL;
+}
+
+void* decrementingThreadFunction(){
+    // TODO: decrement i 1_000_000 times
+    return NULL;
+}
+
+
+int main(){
+    // TODO: 
+    // start the two functions as their own threads using `pthread_create`
+    // Hint: search the web! Maybe try "pthread_create example"?
+    
+    // TODO:
+    // wait for the two threads to be done before printing the final result
+    // Hint: Use `pthread_join`    
+    
+    printf("The magic number is: %d\n", i);
+    return 0;
+}
